f92652de47901fe718bf996098aca07d33ffb3ba
Austin Stewart <aaaustin10@gmail.com>
2014-08-04 14:04:33 -0700

    The things that I can think of right now

---
+++ setup.bash
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get upgrade
+sudo apt-get install tmux htop vim git xchat libxss1 libappindicator1 libindicator7 postgresql
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
