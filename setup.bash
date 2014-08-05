2a56b249de8b14ff74f2db0eb0940a5c28738adf
Austin Stewart <aaaustin10@gmail.com>
2014-08-05 09:59:55 -0500

    Adds some postgres requirements

--- setup.bash
+++ setup.bash
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
-sudo apt-get -y install tmux htop vim git xchat libxss1 libappindicator1 libindicator7 postgresql virtualenvwrapper build-essential libjpeg-dev zlib1g-dev libpng12-dev
-sudo apt-get -y build-dep pillow
+sudo apt-get -y install tmux htop vim git xchat libxss1 libappindicator1 libindicator7 postgresql virtualenvwrapper build-essential libjpeg-dev zlib1g-dev libpng12-dev pgadmin3 python-psycopg2
+sudo apt-get -y build-dep pillow python-psycopg2
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
@@ -14,6 +14,7 @@ cd rcrproject
 source "$(which virtualenvwrapper.sh)"
 mkvirtualenv rcrproject
 pip install -r requirements.txt
+pip install psycopg2
 ./manage.py syncdb
 ./manage.py migrate
 
